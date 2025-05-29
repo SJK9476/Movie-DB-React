0cb32f35ea4153d00254ec7e1dd67db43591e131
Samuel King <samueljamesking94@gmail.com>
2025-05-29 21:24:28 +0100

    Finished Project used to learn ins and outs of React with Tailwwind styling

---
+++ src/appwrite.js
@@ -0,0 +1,51 @@
+import {Client, ID, Query, Databases} from 'appwrite';
+
+const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+
+const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject(PROJECT_ID);
+
+const database = new Databases(client);
+
+
+
+export const updateSearchCount = async (searchTerm, movie) => {
+    // 1. Use Appwrite SDK to check if search term exists in DB
+    // 2. If it does, increment the count by 1
+    // 3. If it doesn't, create a new document with the search term and count of 1
+    try {
+        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [Query.equal('searchTerm', searchTerm)])
+
+        if(result.documents.length > 0) {
+            const document = result.documents[0];
+
+            await database.updateDocument(DATABASE_ID, COLLECTION_ID, document.$id, {
+                count: document.count + 1,
+            })
+        } else {
+            await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(),{
+                searchTerm,
+                count: 1,
+                movie_id: movie.id,
+                poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+            })
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const getTrendingMovies = async () => {
+    try {
+        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+            Query.limit(5),
+            Query.orderDesc("count")
+        ])
+        return result.documents;
+    } catch(error) {
+        console.error(error);
+    }
+}
